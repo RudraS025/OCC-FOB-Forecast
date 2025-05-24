694cdfa858b6618e07ff7a373a1605ccf803f0dc
RudraS025 <rudra.s.25@gmail.com>
2025-05-24 20:21:42 +0530

    Debug: Show real error message in browser for index route

--- app.py
+++ app.py
@@ -54,36 +54,39 @@ def add_new_price(new_date, new_value):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    df = load_data()
-    log_series = np.log(df['OCC_FOB_USD_ton'])
-    lags = [1, 12]
-    model = load_model()
-    forecast = None
-    forecast_dates = None
-    last_month = df.index[-1].strftime('%B %Y')
-    message = None
-    if request.method == 'POST':
-        try:
-            new_value = float(request.form['new_value'])
-            new_date = pd.to_datetime(request.form['new_date'])
-            add_new_price(new_date, new_value)
-            df = load_data()
-            log_series = np.log(df['OCC_FOB_USD_ton'])
-            message = f"Added {new_value} for {new_date.strftime('%B %Y')}!"
-        except Exception as e:
-            message = f"Error: {e}"
-    # Prepare lags for forecast
-    last_logs = list(np.log(df['OCC_FOB_USD_ton'])[-max(lags):])
-    preds = []
-    for i in range(6):
-        features = [last_logs[-lag] for lag in lags]
-        pred_log = model.predict(np.array(features).reshape(1, -1))[0]
-        pred = invert_log_transform(pred_log)
-        preds.append(pred)
-        last_logs.append(pred_log)
-    forecast_dates = [df.index[-1] + DateOffset(months=i+1) for i in range(6)]
-    forecast = list(zip([d.strftime('%b %Y') for d in forecast_dates], preds))
-    return render_template('index.html', forecast=forecast, last_month=last_month, message=message)
+    try:
+        df = load_data()
+        log_series = np.log(df['OCC_FOB_USD_ton'])
+        lags = [1, 12]
+        model = load_model()
+        forecast = None
+        forecast_dates = None
+        last_month = df.index[-1].strftime('%B %Y')
+        message = None
+        if request.method == 'POST':
+            try:
+                new_value = float(request.form['new_value'])
+                new_date = pd.to_datetime(request.form['new_date'])
+                add_new_price(new_date, new_value)
+                df = load_data()
+                log_series = np.log(df['OCC_FOB_USD_ton'])
+                message = f"Added {new_value} for {new_date.strftime('%B %Y')}!"
+            except Exception as e:
+                message = f"Error: {e}"
+        # Prepare lags for forecast
+        last_logs = list(np.log(df['OCC_FOB_USD_ton'])[-max(lags):])
+        preds = []
+        for i in range(6):
+            features = [last_logs[-lag] for lag in lags]
+            pred_log = model.predict(np.array(features).reshape(1, -1))[0]
+            pred = invert_log_transform(pred_log)
+            preds.append(pred)
+            last_logs.append(pred_log)
+        forecast_dates = [df.index[-1] + DateOffset(months=i+1) for i in range(6)]
+        forecast = list(zip([d.strftime('%b %Y') for d in forecast_dates], preds))
+        return render_template('index.html', forecast=forecast, last_month=last_month, message=message)
+    except Exception as e:
+        return f"Internal Server Error: {e}", 500
 
 @app.route('/download')
 def download():
